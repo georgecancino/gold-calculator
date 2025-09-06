e35bdb61b0d8c512308b6f8c8576f13d4e488b89
George Cancino <157549150+georgecancino@users.noreply.github.com>
2025-09-06 13:27:01 -0500

    Update script.js with gold price (09/06/2025)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function calculatePrice() {
     var weight = parseFloat(document.getElementById("weight").value);
     var purity = parseFloat(document.getElementById("purity").value);
-    var goldPricePerGram = 2253 / 31.1034768; // Sample gold price per troy ounce
+    var goldPricePerGram = 3549 / 31.1034768; // Sample gold price per troy ounce
   
     // Calculate purity factor as a decimal value
     var purityFactor = purity / 24;
